d19daebcfd0f4a2458864d42596edf504a332728
jesialanis <jesialanis.1@gmail.com>
2021-10-02 20:21:14 -0300

    Funcion carrito, localStorage y form, css

---
+++ js/destinos.js
@@ -0,0 +1,39 @@
+let arrayViajes = [];
+class Destino {
+
+    constructor(id, nombre, precio, descripcion, imagen, cantidad) {
+        this.id = id;
+        this.nombre =  nombre;
+        this.precio = precio;
+        this. descripcion = descripcion;
+        this. imagen = imagen;
+        this.cantidad = cantidad;
+        
+    }
+
+};
+
+/* Defino objetos*/
+arrayViajes.push(new Destino(1, "Buenos Aires", 9500, "Solo ida","/assets/img/buenos-aires.jpg", 1));
+arrayViajes.push(new Destino(2, "Bariloche", 12000,"Vuelo ida y vuelta", "/assets/img/bariloche.jpg", 1));
+arrayViajes.push(new Destino(3, "Córdoba", 5000, "Solo ida", "/assets/img/cordoba.jpg", 1));
+arrayViajes.push(new Destino(4, "Salta", 10000, "Vuelo ida y vuelta", "/assets/img/salta.jpg", 1));
+arrayViajes.push(new Destino(5, "Iguazú", 15000, "Vuelo ida y vuelta", "/assets/img/iguazu.jpg", 1));
+
+
+let contenedor = document.getElementById("contenedor-productos")
+let acumulador = "";
+
+for (let i = 0; i < arrayViajes.length; i++) {
+   
+    acumulador += ` <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="${arrayViajes[i].imagen}" alt="Card image cap">
+                        <div class="card-body">
+                            <h3>  ${arrayViajes[i].nombre} </h3>
+                            <p> ${arrayViajes[i].descripcion} </p>
+                            <b> $ ${arrayViajes[i].precio} </b>
+                        </div>
+                        <button id=boton${arrayViajes[i].id} type="button" class="btn btn-primary ">Agregar</button>
+                    </div>`;
+}
+contenedor.innerHTML = acumulador;
---
+++ js/main.js
@@ -0,0 +1,93 @@
+let carritoDeCompras = [];
+
+const contenedorProductos = document.getElementById("contenedor-productos");
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+
+const contadorCarrito = document.getElementById("contadorCarrito");
+const precioTotal = document.getElementById("precioTotal")
+
+for (let i = 0; i < arrayViajes.length; i++){
+    let boton = document.getElementById(`boton${arrayViajes[i].id}`)
+    boton.addEventListener("click",()=>{
+        agregarAlCarrito(arrayViajes[i].id)
+    })
+};
+
+
+function agregarAlCarrito(id) {
+    let repetido = carritoDeCompras.find(prodR => prodR.id == id);
+    if(repetido){
+        repetido.cantidad = repetido.cantidad + 1;
+        document.getElementById(`cantidad${repetido.id}`).innerHTML = `<p id="cantidad${repetido.id}">cantidad: ${repetido.cantidad}</p>`
+        actualizarCarrito()
+    }else{
+        let productoAgregar = arrayViajes.find(prod => prod.id == id);
+
+        carritoDeCompras.push(productoAgregar);
+
+        productoAgregar.cantidad = 1;
+        actualizarCarrito()
+        let div = document.createElement('div')
+        div.classList.add('productoEnCarrito')
+        div.innerHTML = `<p>${productoAgregar.nombre}</p>
+                        <p>Precio: ${productoAgregar.precio}</p>
+                        <p id="cantidad${productoAgregar.id}">cantidad: ${productoAgregar.cantidad}</p>
+                        <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                      </svg></button>`
+        contenedorCarrito.appendChild(div)
+        
+        let botonEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+
+        botonEliminar.addEventListener('click', ()=>{
+            botonEliminar.parentElement.remove()
+            carritoDeCompras = carritoDeCompras.filter(prodE => prodE.id != productoAgregar.id)
+            actualizarCarrito()
+        }) 
+    }
+    
+}
+
+
+function actualizarCarrito() {
+   contadorCarrito.innerText = carritoDeCompras.reduce((acc, el)=> acc + el.cantidad,0);
+   precioTotal.innerText = carritoDeCompras.reduce((acc,el)=> acc + (el.precio * el.cantidad),0)
+}
+
+
+
+/*FORMULARIO*/
+document.getElementById("contactForm").addEventListener('submit', enviar);
+let formulario = [];
+
+function enviar(e) {
+    e.preventDefault();
+    /*capturando valores del input*/
+    let nombre = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let celular = document.getElementById('phone').value;
+    let mensaje = document.getElementById('message').value;
+
+    console.log(nombre);
+    console.log(email);
+    console.log(celular);
+    console.log(mensaje);
+
+    let contacto = {
+        nombre,
+        email,
+        celular,
+        mensaje
+    }
+
+    if (localStorage.getItem('contacto') === null) {
+        formulario.push(contacto)
+        localStorage.setItem('contacto', JSON.stringify(formulario));
+    } else {
+        let contacto = JSON.parse(localStorage.getItem('contacto'));
+        formulario.push(contacto)
+        localStorage.setItem('contacto', JSON.stringify(formulario));
+    }
+    document.getElementById('contactForm').reset();
+}
+
