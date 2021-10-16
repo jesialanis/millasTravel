35a079dd6040349be23daa3153509e30e504f986
jesialanis <jesialanis.1@gmail.com>
2021-10-16 17:16:48 -0300

    AJAX

--- js/destinos.js
+++ js/destinos.js
@@ -23,16 +23,23 @@ $.getJSON(URLGET, function (respuesta, estado) {
     }
 })
 
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        for (let i = 0; i < arrayViajes.length; i++) {
 
-for (let i = 0; i < arrayViajes.length; i++) {
-
-    $("#contenedor-productos").append(`<div class="card" style="width: 30rem;">
-                                            <img class="card-img-top" src="${arrayViajes[i].imagen}" alt="Card image cap">
-                                            <div class="card-body">
-                                                <h3>  ${arrayViajes[i].nombre} </h3>
-                                                <p> ${arrayViajes[i].descripcion} </p>
-                                                <b> $ ${arrayViajes[i].precio} </b>
-                                            </div>
-                                            <button id=boton${arrayViajes[i].id} type="button" class="btn btn-primary ">AGREGAR</button>
-                                        </div>`);
+            $("#contenedor-productos").append(`<div class="card" style="width: 30rem;">
+                                                    <img class="card-img-top" src="${arrayViajes[i].imagen}" alt="Card image cap">
+                                                    <div class="card-body">
+                                                        <h3>  ${arrayViajes[i].nombre} </h3>
+                                                        <p> ${arrayViajes[i].descripcion} </p>
+                                                        <b> $ ${arrayViajes[i].precio} </b>
+                                                    </div>
+                                                    <button id=boton${arrayViajes[i].id} onclick="agregarAlCarrito(${arrayViajes[i].id})" type="button" class="btn btn-primary ">AGREGAR</button>
+                                                </div>`);
+        };
+    }
+    xhttp.open("GET", "datos/destinos.json", true);
+    xhttp.send();
 }
+loadDoc();
